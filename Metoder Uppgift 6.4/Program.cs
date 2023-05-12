2d435193ac5076a4cfe83a83220d5812879af65f
Admin <Admin@TEB127006>
2023-05-12 09:13:36 +0200

    Add project files.

---
+++ Metoder Uppgift 6.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Potens(int tal1, int tal2)
+    {
+        Console.WriteLine(tal1 + "^" + tal2 + " = " + (tal1**tal2));
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Tal 1 : ");
+        int tal1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Upphöjt i : ");
+        int tal2 = Convert.ToInt32(Console.ReadLine());
+
+        Potens(tal1, tal2);
+    }
+} 
